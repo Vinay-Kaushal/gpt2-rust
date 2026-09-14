2e6985b2e85f55df941794db92b2c7e9f7fd82f0
Vinay Kaushal <vinaykaushal87439@gmail.com>
2026-09-15 04:59:16 +0530

    Phase 1: read model files

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
-fn main() {
-    let layers = 12;
-    let heads = 12;
-    let dims = 768;
-    let vocab = 50257;
-    println!("Gpt2 small : {layers}layers , {heads}heads, {dims}dims , vocab{vocab}");
+fn main() -> std::io::Result<()> {
+    let merges = std::fs::read_to_string("model/merges.txt")?;
+    let vocab = std::fs::read_to_string("model/vocab.json")?;
+    let model_bytes = std::fs::read("model/model.safetensors")?;
+
+    println!("merges.txt bytes : {}", merges.len());
+    println!("merges line counts: {}", merges.lines().count());
+
+    for line in merges.lines().take(5) {
+        println!("{line}");
+    }
+    println!("vocab.json: {}", vocab.len());
+    println!("model bytes length:{}", model_bytes.len());
+    println!("model bytes in MB : {}", model_bytes.len() / 1_000_000);
+    println!("first 8 bytes: {:?}", &model_bytes[0..8]);
+
+    Ok(())
 }
