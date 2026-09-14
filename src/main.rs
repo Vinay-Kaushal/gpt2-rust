b260ddc025dde786be2e88e831432e346ecc56e7
Vinay Kaushal <vinaykaushal87439@gmail.com>
2026-09-15 01:34:17 +0530

    Phase 0: project setup + hello GPT-2

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let layers = 12;
+    let heads = 12;
+    let dims = 768;
+    let vocab = 50257;
+    println!("Gpt2 small : {layers}layers , {heads}heads, {dims}dims , vocab{vocab}");
+}
